1c46a034f2656383c556b51aa5abfd4129183404
naxx4u <99554823+naxx4u@users.noreply.github.com>
2022-06-14 11:55:57 +0300

    re-work js theme file

--- script/main.js
+++ script/main.js
@@ -1,68 +1,34 @@
-const body = document.querySelector('body')
-const before = document.querySelector('.before')
-const change = document.querySelector('.change_theme')
-const instruments = document.querySelector('.wrapper_instruments')
-const btn = document.querySelector('.btn')
-const btnBlack = document.querySelector('#btnBlack')
-const buyButton = document.querySelector('.footer-buy')
-const footerLanguage = document.querySelector('.footer-language')
-const bgInput = document.querySelector('.footer-style')
-const changeStyleText = document.querySelector('#changeStyleText')
-
-change.addEventListener('click', function(){
-    if(change.checked){
-        body.classList.add('light_theme') + before.setAttribute('style', 'opacity: 0')
-    } else if (!change.checked){
-        body.classList.remove('light_theme') + before.setAttribute('style', 'opacity: 100')
-    }
-})
-
-change.addEventListener('click', function(){
-    if(change.checked){
-        bgInput.classList.add('light_theme') + before.setAttribute('style', 'opacity: 0')
-    } else if (!change.checked){
-        bgInput.classList.remove('light_theme') + before.setAttribute('style', 'opacity: 100')
-    }
-})
-
-change.addEventListener('click', function(){
-    if(!change.checked){
-        changeStyleText.innerHTML = "BLACK" 
-    } else {
-        changeStyleText.innerHTML = "WHITE" 
-    }
-})
-
-
-
+const body = document.querySelector('body'),
+      before = document.querySelector('.before'),
+      change = document.querySelector('.change_theme'),
+      instruments = document.querySelector('.wrapper_instruments'),
+      btn = document.querySelector('.btn'),
+      btnBlack = document.querySelector('#btnBlack'),
+      buyButton = document.querySelector('.footer-buy'),
+      footerLanguage = document.querySelector('.footer-language'),
+      bgInput = document.querySelector('.footer-style'),
+      changeStyleText = document.querySelector('#changeStyleText')
 
 
 change.addEventListener('click', function(){
-    if(change.checked){
-        instruments.classList.add('light_theme') 
-    } else {
-        instruments.classList.remove('light_theme') 
-    }
-})
-change.addEventListener('click', function(){
-    if(change.checked){
-        btn.setAttribute('style', 'opacity: 0 ') + btnBlack.setAttribute('style', 'opacity: 1000') 
-    } else {
-        btn.setAttribute('style', 'opacity: 100') + btnBlack.setAttribute('style', 'opacity: 0') 
-    }
-})
-change.addEventListener('click', function(){
-    if(change.checked){
-        buyButton.classList.add('light_theme') 
-    } else {
-        buyButton.classList.remove('light_theme') 
-    }
-})
-change.addEventListener('click', function(){
-    if(change.checked){
-        footerLanguage.classList.add('light_theme') 
-    } else {
-        footerLanguage.classList.remove('light_theme') 
-    }
-})
+        if(change.checked){
+            body.classList.add('light_theme') + before.setAttribute('style', 'opacity: 0')
+            bgInput.classList.add('light_theme') + before.setAttribute('style', 'opacity: 0')
+            changeStyleText.innerHTML = "BLACK" 
+            instruments.classList.add('light_theme')
+            btn.setAttribute('style', 'opacity: 0 ') + btnBlack.setAttribute('style', 'opacity: 1000')
+            buyButton.classList.add('light_theme')
+            footerLanguage.classList.add('light_theme')
+           
+        } else{
+            body.classList.remove('light_theme') + before.setAttribute('style', 'opacity: 100')
+            bgInput.classList.remove('light_theme') + before.setAttribute('style', 'opacity: 100')
+            changeStyleText.innerHTML = "WHITE"
+            instruments.classList.remove('light_theme') 
+            btn.setAttribute('style', 'opacity: 100') + btnBlack.setAttribute('style', 'opacity: 0')
+            footerLanguage.classList.remove('light_theme') 
+            buyButton.classList.remove('light_theme')
+        }
+    })
+
 
